2f6f03edccfcf36dae0861b9e0ef9134a4055fe7
Frost <moosemashegona@gmail.com>
2025-07-26 19:25:24 +0200

    Updated state from array to object

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import Die from "./MyComponents/Die";
 const App = () => {
   const AllNewDice = () => {
     return new Array(10).fill(0).map(() => {
-      return Math.ceil(Math.random() * 6);
+      return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+      };
     });
   };
   const [dieValue, setDieValue] = useState(AllNewDice());
 
   const diceElements = dieValue.map((die, index) => {
-    return <Die key={index} value={die} />;
+    return <Die key={index} value={die.value} isHeld={die.isHeld} />;
   });
 
   const roll = () => {
