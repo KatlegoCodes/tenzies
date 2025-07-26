621eb3dedd210ae3d0c7a784e87ba3fe54ed8364
Frost <moosemashegona@gmail.com>
2025-07-26 17:33:11 +0200

    Implement basic dice Initialization and component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
+import { useState } from "react";
 import Die from "./MyComponents/Die";
 
 const App = () => {
+  const AllNewDice = () => {
+    return new Array(10).fill(0).map(() => {
+      return Math.ceil(Math.random() * 6);
+    });
+  };
+  const [dieValue, setDieValue] = useState(AllNewDice());
+
+  const diceElements = dieValue.map((die, index) => {
+    return <Die key={index} value={die} />;
+  });
+
   return (
     <>
       <main className="main">
-        <div className="container">
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-        </div>
+        <div className="container">{diceElements}</div>
       </main>
     </>
   );
